5be3f67c5a35fe9040f12a31e18fbb63e79d2283
ark@cloudless.ru <ark@cloudless.ru@474556a4-1cc4-474a-b5e2-4f033f17a488>
2015-04-24 10:00:20 +0000

    from Russia with love (2)
    git-svn-id: https://subversion.assembla.com/svn/scorpozoid/trunk/shell/devel/db/pysdun@638 474556a4-1cc4-474a-b5e2-4f033f17a488

--- ibsql2pgsql.cmd
+++ ibsql2pgsql.cmd
@@ -14,65 +14,85 @@ set PGCLIENTENCODING=utf-8
 rem set PGCLIENTENCODING=WIN  
 rem set PGCLIENTENCODING=WIN1251
 chcp 65001 
-set PGOPTIONS=--client-min-messages=warning
+rem set PGOPTIONS=--client-min-messages=warning
 rem # [i] debug w/echo: -e
 rem # [w] Don't use &PGHOST & $PGUSER variables here: -h %PGHOST% -U %PGUSER% -n -v ON_ERROR_STOP=1
-set PGPSQL_OPTS=-n -v ON_ERROR_STOP=1 -q
-
+rem set PGPSQL_OPTS=-n -v ON_ERROR_STOP=1 -q
+set PGPSQL_OPTS=
 
 set PROJ418=%HOME%\devel\nig\dfpost\418
 set PROJ419=%HOME%\devel\nig\dfpost\419
 set PROJXE3=%HOME%\devel\nig\dfpost\xe3
 
-
-rem # --
-rem # -- 8.2 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-rem # --
-set PGUSER=postgres
-set PGPASSWORD=postgres
-rem set PGPASSWORD=masterkey
-set PGHOST=192.168.47.34
-rem set PGHOSTADDR=192.168.47.134
-set PGPSQL_EXE="%HOME%\bin\pgAdmin III 1.10.5\psql.exe" 
-set PGPSQL=%PGPSQL_EXE% %PGPSQL_OPTS%
-
-rem # - 8.2 / 4.1.8 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+rem # - 4.1.8 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 set IBESCRIPT418=%PROJ418%\trunk\DFLib\DataBase\dfPostDB(IB)\dfPostDB.sql
 set PGSCRIPT418MAIN=%PROJ418%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql.sql
 set PGSCRIPT418SP=%PROJ418%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-proc.sql
 set PGSCRIPT418TR=%PROJ418%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-triggers.sql
-set DB418=dfpostdb418
-python ibsql2pgsql.py -i %IBESCRIPT418% -o %PGSCRIPT418MAIN%
-%PGPSQL% -c "drop database %DB418%"
-%PGPSQL% -c "create database %DB418%"
-%PGPSQL% -d %DB418% -c "create language 'plpgsql'"
-%PGPSQL% -d %DB418% -f %PGSCRIPT418MAIN%
-%PGPSQL% -d %DB418% -f %PGSCRIPT418SP%
-%PGPSQL% -d %DB418% -f %PGSCRIPT418TR%
-echo "FIN: 8.2/4.1.8"
-pause
-rem exit /b
+set PGSCRIPT418TEST01=%PROJ418%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-test-01.sql
+set PGSCRIPT418TEST02=%PROJ418%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-test-02.sql
 
-rem # - 8.2 / 4.1.9 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+python ibsql2pgsql.py -i %IBESCRIPT418% -o %PGSCRIPT418MAIN%
+
+rem # - 4.1.9 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 set IBESCRIPT419=%PROJ419%\trunk\DFLib\DataBase\dfPostDB(IB)\dfPostDB.sql
 set PGSCRIPT419MAIN=%PROJ419%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql.sql
 set PGSCRIPT419SP=%PROJ419%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-proc.sql
 set PGSCRIPT419TR=%PROJ419%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-triggers.sql
-set DB419=dfpostdb419
+set PGSCRIPT419TEST01=%PROJ419%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-test-01.sql
+set PGSCRIPT419TEST02=%PROJ419%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-test-02.sql
+
 python ibsql2pgsql.py -i %IBESCRIPT419% -o %PGSCRIPT419MAIN%
+
+copy %PGSCRIPT418SP% %PGSCRIPT419SP%
+copy %PGSCRIPT418TR% %PGSCRIPT419TR%
+copy %PGSCRIPT418TEST01% %PGSCRIPT419TEST01%
+copy %PGSCRIPT418TEST02% %PGSCRIPT419TEST02%
+
+rem # --
+rem # -- 8.2 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+rem # --
+set PGHOST=192.168.47.134
+set PGHOSTADDR=192.168.47.134
+set PGUSER=postgres
+set PGPASSWORD=postgres
+rem set PGPASSWORD=masterkey
+set PGPSQL_EXE="%HOME%\bin\pgAdmin III 1.10.5\psql.exe" 
+set PGPSQL=%PGPSQL_EXE% %PGPSQL_OPTS%
+
+rem # - 8.2 / 4.1.8 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+set DB418=dfpostdb418
+%PGPSQL% -c "drop database %DB418%"
+%PGPSQL% -c "create database %DB418%"
+%PGPSQL% -d %DB418% -c "create language plpgsql"
+%PGPSQL% -d %DB418% -f %PGSCRIPT418MAIN%
+%PGPSQL% -d %DB418% -f %PGSCRIPT418SP%
+%PGPSQL% -d %DB418% -f %PGSCRIPT418TR%
+%PGPSQL% -d %DB418% -f %PGSCRIPT418TEST01%
+%PGPSQL% -d %DB418% -f %PGSCRIPT418TEST02%
+echo "FIN: 8.2/4.1.8 %PGHOST%::%DB418%"
+pause
+rem exit /b
+
+rem # - 8.2 / 4.1.9 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+set DB419=dfpostdb419
 %PGPSQL% -c "drop database %DB419%"
 %PGPSQL% -c "create database %DB419%"
-%PGPSQL% -d %DB419% -c "create language 'plpgsql'"
+%PGPSQL% -d %DB419% -c "create language plpgsql"
 %PGPSQL% -d %DB419% -f %PGSCRIPT419MAIN%
 %PGPSQL% -d %DB419% -f %PGSCRIPT419SP%
 %PGPSQL% -d %DB419% -f %PGSCRIPT419TR%
-echo "FIN: 8.2/4.1.9"
+%PGPSQL% -d %DB419% -f %PGSCRIPT419TEST01%
+%PGPSQL% -d %DB419% -f %PGSCRIPT419TEST02%
+echo "FIN: 8.2/4.1.9 %PGHOST%::%DB419%"
 pause
-
+rem exit /b
 
 rem # --
 rem # -- 9.1 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 rem # --
+set PGHOST=192.168.47.38
+set PGHOSTADDR=192.168.47.38
 set PGUSER=postgres
 set PGPASSWORD=masterkey
 set PGHOSTADDR=192.168.47.38
@@ -80,35 +100,29 @@ set PGPSQL_EXE="%HOME%\bin\pgAdmin III 1.20.0\psql.exe"
 set PGPSQL=%PGPSQL_EXE% %PGPSQL_OPTS%
 
 rem # - 9.1 / 4.1.8 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-set IBESCRIPT418=%PROJ418%\trunk\DFLib\DataBase\dfPostDB(IB)\dfPostDB.sql
-set PGSCRIPT418MAIN=%PROJ418%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql.sql
-set PGSCRIPT418SP=%PROJ418%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-proc.sql
-set PGSCRIPT418TR=%PROJ418%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-triggers.sql
 set DB418=dfpostdb418
-python ibsql2pgsql.py -i %IBESCRIPT418% -o %PGSCRIPT418MAIN%
 %PGPSQL% -c "drop database %DB418%"
 %PGPSQL% -c "create database %DB418%"
 %PGPSQL% -d %DB418% -c "create language 'plpgsql'"
 %PGPSQL% -d %DB418% -f %PGSCRIPT418MAIN%
 %PGPSQL% -d %DB418% -f %PGSCRIPT418SP%
 %PGPSQL% -d %DB418% -f %PGSCRIPT418TR%
-echo "FIN: 9.1/4.1.8"
+%PGPSQL% -d %DB418% -f %PGSCRIPT418TEST01%
+%PGPSQL% -d %DB418% -f %PGSCRIPT418TEST02%
+echo "FIN: 9.1/4.1.8 %PGHOST%::%DB418%"
 pause
 
 rem # - 9.1 / 4.1.9 - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
-set IBESCRIPT419=%PROJ419%\trunk\DFLib\DataBase\dfPostDB(IB)\dfPostDB.sql
-set PGSCRIPT419MAIN=%PROJ419%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql.sql
-set PGSCRIPT419SP=%PROJ419%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-proc.sql
-set PGSCRIPT419TR=%PROJ419%\trunk\DFLib\DataBase\dfPostDB(PGS)\dfpostdb-pgsql-triggers.sql
 set DB419=dfpostdb419
-python ibsql2pgsql.py -i %IBESCRIPT419% -o %PGSCRIPT419MAIN%
 %PGPSQL% -c "drop database %DB419%"
 %PGPSQL% -c "create database %DB419%"
 %PGPSQL% -d %DB419% -c "create language 'plpgsql'"
 %PGPSQL% -d %DB419% -f %PGSCRIPT419MAIN%
 %PGPSQL% -d %DB419% -f %PGSCRIPT419SP%
 %PGPSQL% -d %DB419% -f %PGSCRIPT419TR%
-echo "FIN: 9.1/4.1.9"
+%PGPSQL% -d %DB419% -f %PGSCRIPT419TEST01%
+%PGPSQL% -d %DB419% -f %PGSCRIPT419TEST02%
+echo "FIN: 9.1/4.1.9 %PGHOST%::%DB419%"
 pause
 
 rem # /* EOF */
