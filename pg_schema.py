3d73fb07d8b9c86d4a294449e1ac4bb5fa4bdbb4
ark@cloudless.ru <ark@cloudless.ru@474556a4-1cc4-474a-b5e2-4f033f17a488>
2018-03-31 20:42:29 +0000

    [*] temporary
    git-svn-id: https://subversion.assembla.com/svn/scorpozoid/trunk/shell/devel/db/pysdun@1054 474556a4-1cc4-474a-b5e2-4f033f17a488

--- pg_schema.py
+++ pg_schema.py
@@ -21,11 +21,64 @@ from ibe_ddl import StoredProcedure
 from ibe_ddl import Trigger
 
 
+def debug(msg):
+    try:
+        print(msg.encode(sys.stdout.encoding, errors='replace'))
+    except TypeError:
+        print("NONE")
+
+
 class PgSchema(Schema):
 
     def __init__(self, filename):
         Schema.__init__(self)
-        #self.__re_set_term = None
-        #self.__lines = []
-        #self.__statements = []
-        #self.load(filename)
+        self.__lines = []
+        self.__statements = []
+        self.load(filename)
+
+    def load(self, filename):
+        try:
+            #self.__lines = [line for line in codecs.open(filename, encoding='cp1251')]
+            self.__lines = [line for line in codecs.open(filename)]
+            print(self.__lines)
+            self.prepare_statements()
+            #for statement in self.__statements:
+            #    debug(statement)
+        except IOError:
+            print('Can\'t open the "{0}" file'.format(filename))
+
+    def prepare_statements(self):
+        term = ';'
+        buf = ''
+        comment = False
+        for line in self.__lines[:]:
+            line = line.strip()
+            #debug(line)
+            x = line.find('--')
+            if -1 < x:
+                line = line[:x]
+            x1 = line.find('/*')
+            x2 = line.find('*/')
+            if (-1 < x1) or (-1 < x2):
+                if (-1 < x1) and (-1 < x2):
+                    line = line[:x1] + line[x2 + 2:]
+                else:
+                    if -1 < x1:
+                        line = line[:x1]
+                        comment = True
+                    else:
+                        line = line[x2 + 2:]
+                        comment = False
+
+            if ('' == line) or comment:
+                continue
+            buf = buf + line + ' '
+            if term in buf.lower():
+                buf = re.sub('[\t+\r+\n+]', ' ', buf).strip()
+                buf = buf.replace('( ', '(')
+                buf = buf.replace('  ', ' ')
+                if buf.lower().startswith('insert'):
+                    self.__statements.append(buf)
+                else:
+                    self.__statements.append(buf.lower())
+                buf = ''
